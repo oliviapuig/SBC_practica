f481f6830ea06e83e433c189288be019869eb127
ruthparajo <118297984+ruthparajo@users.noreply.github.com>
2023-12-12 15:27:51 +0100

    hola

---
+++ prototip3-provesruth/prototip3.py
@@ -0,0 +1,34 @@
+# # SBC - Pràctica CBR
+# 
+# - Aina Gomila
+# - Ruth Parajó
+# - Olivia Puig
+# - Marc Ucelayeta
+
+from cbr import CBR
+from utils import Usuari
+import pickle
+import pandas as pd
+import random
+import numpy as np
+
+cases =pd.read_pickle('../data/casos.pkl')
+books = pd.read_pickle('../data/llibres.pkl')
+
+with open('../data/clustering/model_clustering_kmeans.pkl', 'rb') as arxiu:
+    clustering = pickle.load(arxiu)
+
+#estableixo nou cas com el primer borrant els seus llibres recomanats (temporal)
+cases.llibres_recomanats[0] = []
+cases.puntuacions_llibres[0] = []
+nou_cas = cases.iloc[0]
+cases=cases.iloc[1:]
+
+
+
+cbr = CBR(cases,clustering,books)
+recomanacio = cbr.recomana(nou_cas)
+print("Usuari final:")
+print(recomanacio)
+
+
